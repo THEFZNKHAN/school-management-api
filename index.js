12ad23f096d53dbadf22a1543a60d7325471123b
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2025-08-09 20:32:04 +0530

    feat: Add static file serving and initial HTML for School Management API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path'); // Add path module
 const routes = require('./src/api/routes');
 const client = require('./src/config/database');
 const errorHandler = require('./src/middleware/errorHandler');
@@ -9,6 +10,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.use('/api', routes);
 app.use(errorHandler);
 
